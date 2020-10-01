120f247875150f7a87d04ee8adb2e0236c4ca541
CarlosReyesPena <carlosa.reyespena@heig-vd.ch>
2020-10-01 15:13:26 +0200

    Halft projet

--- main.cpp
+++ main.cpp
@@ -2,9 +2,19 @@
 #include <iostream>
 #include <iomanip>
 
+
+void Verticalbord(const char coin, const char vertical, const char W);
+
+void spacetable(const char space, const char horizontal, const char W);
+
 using namespace std;
 
 int main() {
+
+    string n = "test 1";
+    string i = "test 2";
+    cout.width(30); cout << left << n << endl;
+    cout.width(30); cout << right << n << endl;
 
     cout << "Quel uber? Entrez 1 pour X, 2 pour POP ou 3 pour BLACK" << endl;
     int ChoixUber = 0;
@@ -19,31 +29,49 @@ int main() {
     cin >> MinutesEcoulees;
 
     // affichage de quel uber choisi
-    const string U = "uber";
-    string uber;
+    const string fixedUber = "uber";
+    string typeUber;
     //string uber = ChoixUber = 1? U + "X" : ChoixUber = 2? U + "POP" : U + "BLACK"; pourquoi ne marche pas?
     if (ChoixUber == 1) {
-        uber = U + "X";
+        typeUber =  "X";
     } else if (ChoixUber == 2) {
-        uber = U + "POP";
+        typeUber =  "POP";
     } else {
-        uber = U + "BLACK";
+        typeUber =  "BLACK";
     }
-    cout << uber << endl;
-
+    const char space = ' ';
     //affichage course annulée
-    const int W = 28;
+    const char coin = '+';
+    const char vertical = '-';
+    const char horizontal = '|';
+    const char W = 28;
+    const char T = 30;
+
+
+    const char SEPARATOR = ':';
+    const char E = 16;
+    const char WIDHT_NUM = 6;
+    cout << fixed << setprecision(2);
     if (MinutesEcoulees < 1)
     {
-        cout << '+' << setfill('-') << setw(W) << '+' << endl;
-        for (int i = 0; i < 8; i++){
-            if (i == 1){
-                cout << '|' << uber << setw(W) << '|' << endl; //comment centrer?
-            }
-            cout << '|' << setfill(' ') << setw(W) << '|' << endl;
-        }
-        cout << '+' << setfill('-') << setw(W) << '+' << endl;
+        double annulationfrais = 0;
+        switch (ChoixUber) {
+            case 1 :
+            case 2 :
+                annulationfrais = 6.00;
+                break;
+            case 3 :
+                annulationfrais = 10.00;
 
+        }
+
+        Verticalbord(coin, vertical, W);
+        spacetable(space, horizontal, W);
+        cout << horizontal << right << setw(W/2) << fixedUber  << left << setw(T) << typeUber << horizontal << endl; //comment centrer?
+        cout << horizontal << setfill(space) << setw(W) << horizontal << endl;
+        cout << horizontal << space << left <<setw(E) << "Frais annulation"<< space << SEPARATOR
+        << space << right << setw(WIDHT_NUM) << annulationfrais << space << horizontal << endl;
+        Verticalbord(coin, vertical, W);
 
     }
     else
@@ -59,23 +87,33 @@ int main() {
     cout << "prix" << endl;
     cin >> prix;
     //affichage course
-    cout << '+' << setfill('-') << setw(W) << '+' << endl;
-        if ((1 || 2 || 3) && prix < min) {
-            for (int i = 0; i < 16; i++) {
-                if (i == 1){
-                    cout << '|' << uber << setw(W) << '|' << endl; //comment centrer?
-                }
-                cout << '|' << setfill(' ') << setw(W) << '|' << endl;
-            }
-        } else{
+
+    cout << coin << setfill(vertical) << setw(W) << coin << endl;
+    cout << horizontal << setfill(' ') << setw(W) << horizontal << endl;
+    //int test = uber.length();
+    // int UberWidth = (W-test-1)/2;
+    cout << internal << horizontal << setw(14) << typeUber  << setw(14) << horizontal << endl; //comment centrer?
+    cout << horizontal << setfill(' ') << setw(W) << horizontal << endl;
+    cout << horizontal << setfill(' ') << setw(W) << horizontal << endl;
+
+
+
+
             for (int i = 0; i < 14; i++) {
                 if (i == 1){
-                    cout << '|' << uber << setw(W) << '|' << endl; //comment centrer?
+
+                    cout << horizontal << typeUber << horizontal << internal << endl; //comment centrer?
                 }
-                cout << '|' << setfill(' ') << setw(W) << '|' << endl;
+                cout << horizontal << setfill(' ') << setw(W) << horizontal << endl;
             }
-        }
-    cout << '+' << setfill('-') << setw(W) << '+' << endl;
+
+    cout << coin << setfill(vertical) << setw(W) << coin << endl;
 
     return 0;
+}
+
+void spacetable(const char space, const char horizontal, const char W) { cout << horizontal << setfill(space) << setw(W) << horizontal << endl; }
+
+void Verticalbord(const char coin, const char vertical, const char W){
+    cout << coin << setfill(vertical) << setw(W) << coin << endl;
 }
